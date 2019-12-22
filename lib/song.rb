891211ac1c8cdb85ea730b303c3f551bc7e79c87
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-22 03:10:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,7 +43,7 @@ class Song
   
   def self.artist_count
     artist_count = Hash.new(0)
-    @@artists.each => {|artist| artist_count[artist] += 1}
+    @@artists.each  {|artist| artist_count[artist] += 1}
     artist_count
   end
 end
