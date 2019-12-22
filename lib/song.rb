f7893423202d878d5661899eb238789a3403fffa
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-22 04:34:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,37 +6,38 @@ class Song
   @@genres = []
   
   def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @genre = genre
-    @@count += 1
-    @@artists << artist
-    @@genres << genre
+      @name = name
+      @artist = artist
+      @genre = genre
+      @@count += 1
+      @@artists << artist
+      @@genres << genre
   end
   
   def self.count
-    @@count
+      @@count
   end
   
   def self.artists
-    @@artists.uniq
+      @@artists.uniq
   end
   
   def self.genres
-    @@genres.uniq
+      @@genres.uniq
   end
   
   
   def self.genre_count
-    genre_count = {}
-    @@genres.each do  |genre|
-      if  genre_count[genre]
-          genre_count[genre] += 1
-      else
-         genre_count[genre] = 1
+      genre_count = {}
+      @@genres.each do  |genre|
+        if  genre_count[genre]
+            genre_count[genre] += 1
+        else
+            genre_count[genre] = 1
+        end
       end
-    end
-  genre_count
+      genre_count
+  end
   
  
   def self.artist_count
