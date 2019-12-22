8bdd6ed87a7e8b5e803d5dc67c97523b63e0d842
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-22 03:02:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,7 +36,7 @@ class Song
   
   def self.genre_count
     genre_count = Hash.new(0)
-    @@genres.each {|genre|  genre_count|genre| += 1}
+    @@genres.each {|genre|  genre_count|genre|}
     genre_count
   end
   
