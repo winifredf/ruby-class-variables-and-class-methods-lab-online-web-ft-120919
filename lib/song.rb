d87086a2bfce3765aa91bfdc2e1cbdc14f79cbf6
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-22 04:29:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,7 @@ class Song
     genre_count = {}
     @@genres.each do  |genre|
       if  genre_count[genre]
-          genre_count += 1
+          genre_count[genre] += 1
       else
          genre_count = 1
       end
