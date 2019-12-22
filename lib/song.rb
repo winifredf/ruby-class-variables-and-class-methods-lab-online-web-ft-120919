37dd01e7cc99fd2d6d5def842349bd0c9accd662
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-22 04:15:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,7 +43,7 @@ class Song
     artist_count = {}
     @@artists.each  do  |artist|
       if  artist_count[artist]
-          artist_count[artist] += 1
+            artist_count[artist] += 1
       else  artist_count[artist] = 1
       end
     end
